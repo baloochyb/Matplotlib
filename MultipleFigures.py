df363c58190d788d5ec441671dc5f95cb36380ef
Behnam Baloochy <baloochy@gmail.com>
2023-06-07 07:15:28 +0330

    MultipleFigures

---
+++ MultipleFigures.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+fig, axd = plt.subplot_mosaic([['upleft', 'right'], ['lowleft', 'right']], layout='constrained')
+axd['upleft'].set_title('upleft')
+axd['lowleft'].set_title('lowleft')
+axd['right'].set_title('right')
+
+plt.show()
